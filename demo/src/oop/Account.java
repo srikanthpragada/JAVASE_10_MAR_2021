9411c4a1ca5aac1624901d5ee633fb858d903693
srikanthpragada <srikanthpragada@gmail.com>
2021-03-19 18:56:34 +0530

    19-MAR

--- demo/src/oop/Account.java
+++ demo/src/oop/Account.java
@@ -1,28 +1,42 @@
 package oop;
 
 public class Account {
-   // instance variables 
-   private int acno;
-   private String ahname;
-   private double balance;
-   
-   // constructor
-   public Account(int no, String name) {
-	   acno = no;
-	   ahname = name;
-	   balance = 0;
-   }
-   // Methods 
-   public void print() {
-	   System.out.printf("%d %s %f\n", acno,ahname,balance);
-   }
-   
-   public void deposit(double amount) {
-	   balance += amount;
-   }
-   
-   public void withdraw(double amount) {
-	   balance -= amount;
-   }
-   
+	// instance variables
+	private int acno;
+	private String ahname;
+	private double balance;
+	// class variable
+	private static int minbal = 5000;
+
+	// constructor
+	public Account(int no, String name) {
+		acno = no;
+		ahname = name;
+		balance = 0;
+	}
+
+	// Methods
+	public void print() {
+		System.out.printf("%d %s %f\n", acno, ahname, balance);
+	}
+
+	public void deposit(double amount) {
+		balance += amount;
+	}
+
+	public void withdraw(double amount) {
+		if (balance - minbal >= amount)
+			balance -= amount;
+		else
+			System.out.println("Insufficient Balance!");
+	}
+	
+	public double getBalance() {
+		return this.balance; 
+	}
+	
+	static public int getMinBalance() {
+		return  minbal; 
+	}
+
 }
