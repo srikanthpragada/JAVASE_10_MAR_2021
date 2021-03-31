e7c56dfbf3ff0f33fef6a531e66840be6068bcd6
srikanthpragada <srikanthpragada@gmail.com>
2021-03-31 18:58:24 +0530

    31-MAR

--- demo/src/library/TestPerson.java
+++ demo/src/library/TestPerson.java
@@ -15,9 +15,19 @@ class Person
 		return this.name + "," + this.age;
 	}
 	
+	public int hashCode() {
+		return this.age;
+	}
+	
 	@Override 
 	public boolean equals(Object obj) {
-		return false; 
+		if(obj instanceof Person) 
+		{
+		  Person other = (Person) obj;  // Downcasting
+		  return this.name.equals(other.name) && this.age == other.age;
+		}
+		else
+			return false;
 	}
 }
 public class TestPerson {
@@ -25,7 +35,11 @@ public class TestPerson {
 	public static void main(String[] args) {
 		 Person p1 = new Person("James",50);
 		 Person p2 = new Person("James",50);
+		 System.out.println(p1.hashCode());
+		 System.out.println(p2.hashCode());
+		 
 		 System.out.println(p1.equals(p2));
+		 System.out.println(p1.equals("Abc"));
 		 System.out.println(p1.toString());
 	}
 }
